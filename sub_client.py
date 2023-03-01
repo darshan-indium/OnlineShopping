0ae528fadf708be9bd16a89dfbbf9f10a7e45553
darshan-indium <darshan.r@indiumsoft.com>
2023-03-01 12:18:31 +0530

    Create NPC

---
+++ sub_client.py
@@ -0,0 +1,13 @@
+import socket
+
+c=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+c.connect(("localhost",4000))
+
+while True:
+    data=input("Enter a word: ")
+    c.send(bytes(data,"UTF-8"))
+    data2=input("To check if substring or not")
+    c.send(bytes(data2,"UTF-8"))
+    value=c.recv(1024)
+    print(value.decode())
